55264392c5e97f27044204df0d1b854d0f8ed5ae
doespresso <itone.ltd@gmail.com>
2014-06-26 23:45:05 +0400

    add old assets and view

---
+++ app/views/home/old.blade.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link rel="shortcut icon" href="../favicon.ico" />
+    <title>Ресторан Leroy в Park Inn by Radisson, Одинцово</title>
+    <link rel="stylesheet" href="/oldasset/css/fontello-embedded.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="../oldasset/css/css.css" type="text/css" media="screen" />
+    <script type="text/javascript" src="../oldasset/jscript/jquery-1.8.1.min.js"></script>
+    <script type="text/javascript" src="../oldasset/jscript/vendor/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../oldasset/jscript/core.js"></script>
+</head>
+
+<body class="homebody">
+
+<header>
+
+</header>
+
+<article>
+    <div id="clogo">
+        <img src="../oldasset/graphic/logo_w.svg"/>
+    </div>
+    <!--<div id="open">-->
+    <!--<div class="digit"></div><div class="do">дня<br>до открытия<br>сайта</div>-->
+    <!--</div>-->
+    <div id="cadr">
+        <p class="naming">R E S T A U R A N T<br>B A R & G R I L L</p>
+        <hr>
+        <p class="adress">Odintsovo, Nedelina St. 8<br>Park Inn by Radisson</p>
+        <hr>
+        <p class="adress">Questions & Reserv<br>+7 495 594 16 06</p>
+    </div>
+    <div style="margin-top: 0px; margin-left:60px; line-height:42px; border-bottom:1px solid #fff; font-size:24px; padding:10px 60px;">&mdash;
+        <a href="/oldassetfiles/LEROY_CORPORATE_OFFER.pdf" target="_blank" style="color:#fff; text-decoration: none;">праздники и корпоративы в ресторане Leroy</a>
+    </div>
+</article>
+
+</body>
+</html>
